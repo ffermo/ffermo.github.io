a4f2a8d713383c4f2d8cd8ae580b7e4cfa4cf25b
Francis Fermo <fermo.francis@gmail.com>
2024-08-13 12:55:58 -0600

    Upgraded Angular, packages, and dependencies.

---
+++ build/src/util/location.util.ts
@@ -0,0 +1,12 @@
+export interface Coordinates {
+  lon: number;
+  lat: number;
+}
+
+export const QUEZON_PH_LOC: Coordinates = { lon: 121, lat: 14.4 };
+export const ORLANDO_FL_LOC: Coordinates = { lon: -81.2, lat: 28.3 };
+
+export function isNearTarget(local: Coordinates, target: Coordinates): boolean {
+  return Math.abs(local.lat - target.lat) < .5 && 
+    Math.abs(local.lon - target.lon) < .5
+}
---
+++ build/src/util/scene.util.ts
@@ -0,0 +1,9 @@
+export const GALAXY_TEXTURE_PATH: string = 'assets/textures/milkyway.jpg';
+export const EARTH_TEXTURE_PATH: string = 'assets/textures/earth.jpg';
+export const EARTH_NORMAL_PATH: string = 'assets/textures/earth_normal.jpg';
+export const EARTH_SPEC_PATH: string = 'assets/textures/earth_spec.jpg';
+export const CLOUDS_TEXTURE_PATH: string = 'assets/textures/earth_clouds.jpg';
+
+export const CAMERA_FOV: number = 60;
+export const MAX_CAMERA_DIST: number = 48;
+export const MIN_CAMERA_DIST: number = 2;
